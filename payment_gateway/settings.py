885da4b14373c79a4c2696ed5ce7eb3172bc0987
Doradino <doradino35@gmail.com>
2025-01-23 18:11:11 +0100

    Fix deployment directory for Netlify static files

--- payment_gateway/settings.py
+++ payment_gateway/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 # Initialize the environment variables
 env = environ.Env()
@@ -132,7 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
